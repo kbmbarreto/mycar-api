20c7185c6dbf6cff4ce4511d5cbed6284c504e8c
cskmacedo <kleber.barreto@msn.com>
2023-01-12 16:39:19 -0300

    Terminadas as rotas de components

--- routes/components.js
+++ routes/components.js
@@ -1,12 +1,57 @@
 module.exports = app => {
     const Components = app.models.components;
 
-    app.get('/components', async (req, res) => {
-        try {
-            const components = await Components.findAll();
-            res.json({components});
-        }catch (ex) {
-            res.status(500).json(ex);
-        }
-    });
+    app.route('/components')
+        .get(async (req, res) => {
+            try {
+                const result = await Components.findAll();
+                res.json(result);
+            } catch (ex) {
+                res.status(412).json({msg: ex.message});
+            }
+        })
+        .post(async (req, res) => {
+            try {
+                const result = await Components.create(req.body);
+                res.json(result);
+            } catch (ex) {
+                res.status(412).json({msg: ex.message});
+            }
+        });
+
+    app.route('/components/:id')
+        .get(async (req, res) => {
+            try {
+                const {id} = req.params;
+                const where = {id};
+                const result = await Components.findOne({where});
+                if (result) {
+                    res.json(result);
+                } else {
+                    res.sendStatus(404);
+                }
+            } catch (ex) {
+                res.status(412).json({msg: ex.message});
+            }
+        })
+        .put(async (req, res) => {
+            try {
+                const {id} = req.params;
+                const where = {id};
+                await Components.update(req.body, {where});
+                res.sendStatus(204);
+            } catch (ex) {
+                res.status(412).json({msg: ex.message});
+            }
+        })
+        .delete(async (req, res) => {
+            try {
+                const {id} = req. params;
+                const where = {id};
+                await Components.destroy({where});
+                res.sendStatus(204);
+            } catch (ex) {
+                res.status(412).json({msg: ex.message});
+            }
+        });
 };
