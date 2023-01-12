b8d8b00bcdc758b81e6bed8bc9fc88187124ec15
cskmacedo <kleber.barreto@msn.com>
2023-01-11 22:12:47 -0300

    Adicionada a conexão com banco de dados

--- boot.js
+++ boot.js
@@ -1,5 +1,15 @@
 module.exports = app => {
-    app.listen(app.get('port'), () => {
-        console.log(`MyCar API - port ${app.get('port')}`);
-    });
+    async function start(port) {
+        try {
+            await app.db.authenticate();
+            await app.db.sync();
+            app.listen(app.get('port'), () => {
+                console.log(`MyCar API - port ${app.get('port')}`);
+            });
+        } catch (ex) {
+            console.log('Database connection error!');
+            console.error(ex);
+        }
+    }
+    start(app.get('port'));
 }
---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    db: {
+        database: 'mycar',
+        username: 'root',
+        password: 'Dev2020@',
+        params: {
+            dialect: 'mysql',
+            define: {
+                underscored: true
+            }
+        }
+    }
+};
